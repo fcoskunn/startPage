de63b9de3b5b130b1765b3f858697feff93b3e40
Fatih Coşkun <fatih.coskun@agu.edu.tr>
2019-07-13 19:47:55 +0300

    Initialization

---
+++ script.js
@@ -0,0 +1,141 @@
+const NAME = "Fatih";
+const WELCOME_MESSAGE_TEMPLATE = ["night", "morning", "afternoon", "evening"];
+
+// All shortcuts are in a `SHORTCUT_STARTER+shortcutKey` format. 
+// So, for example, pressing `tab+q` would redirect you to https://google.com/?q=q
+const SHORTCUT_STARTER = 'tab' 
+
+// How much time (in milliseconds) you have to press shortcutKey after pressing SHORTCUT_STARTER.
+// Also change --SHORTCUT_TIMEOUT in styles.css if you change this option.
+const SHORTCUT_TIMEOUT = 1500;
+
+// The groups of links are generated from this object. Edit it to edit the page's contents.
+// shortcutKey must hold an all-lowercase single button. Theoretically should work with values like `esc` and `f1`,
+// but intended to be used with just regular latin letters.
+const MASTER_MAP = [
+    {
+        "groupName": "Daily",
+        "items":[
+            {"name": "Gmail", "shortcutKey": "q", "url": "https://inbox.google.com/"},
+            {"name": "Github", "shortcutKey": "w", "url": "https://github.com/"},
+            {"name": "Canvas", "shortcutKey": "e", "url": "https://canvas.agu.edu.tr/"}
+        ]
+    },
+    {
+        "groupName": "Media",
+        "items":[
+            {"name": "Youtube", "shortcutKey": "a", "url": "https://youtube.com/"},
+            {"name": "Netflix", "shortcutKey": "s", "url": "https://netflix.com/"},
+            {"name": "Reddit", "shortcutKey": "d", "url": "https://reddit.com/"}
+        ]
+    },
+    {
+        "groupName": "Social",
+        "items":[
+            {"name": "Twitter", "shortcutKey": "z", "url": "https://twitter.com/"},
+            {"name": "Facebook", "shortcutKey": "x", "url": "https://facebook.com/"},
+            {"name": "Whatsapp Web", "shortcutKey": "c", "url": "https://web.whatsapp.com/"}
+        ]
+    }
+]
+
+let $container = document.getElementById("content");
+let getUrl = {};
+
+let $shortcutDisplayList = document.getElementsByClassName("shortcut");
+let listeningForShortcut = false;
+let listenerTimeout;
+
+function setupWelcomeMessage(){
+    let curHours = new Date().getHours();
+    curHours = Math.floor(curHours/6); // Simply dividing current hours by 6 proves to be a good enough aproximation.
+    if (curHours == 4) curHours = 3;
+    let welcome = "Good " + WELCOME_MESSAGE_TEMPLATE[curHours] + ", " + NAME;
+    document.getElementById("welcome-string").innerHTML = welcome;
+}
+
+function setupGroups(){
+    for (let i = 0; i < MASTER_MAP.length; i++){
+        let curGroupData = MASTER_MAP[i];
+
+        let group = document.createElement("div");
+        group.className = "group";
+        $container.appendChild(group);
+
+        let header = document.createElement("h1");
+        header.innerHTML = curGroupData.groupName;
+        group.appendChild(header);
+
+        for (let j = 0; j < curGroupData.items.length; j++){
+            let curItemData = curGroupData.items[j];
+
+            let pContainer = document.createElement("p");
+            group.appendChild(pContainer);
+
+            let link = document.createElement("a");
+            link.innerHTML = curItemData.name;
+            link.setAttribute("href", curItemData.url);
+            pContainer.appendChild(link);
+
+            let shortcutDisplay = document.createElement("span");
+            shortcutDisplay.innerHTML = curItemData.shortcutKey;
+            shortcutDisplay.className = "shortcut";
+            shortcutDisplay.style.animation = "none";
+            pContainer.appendChild(shortcutDisplay);
+
+            getUrl[curItemData.shortcutKey] = curItemData.url
+        }
+    }
+}
+
+function shortcutListener(e) {
+    let key = e.key.toLowerCase();
+
+    if (listeningForShortcut && getUrl.hasOwnProperty(key)){
+        window.location = getUrl[key];
+    }
+
+    if (key === SHORTCUT_STARTER) {
+        clearTimeout(listenerTimeout);
+        listeningForShortcut = true;
+
+        // Animation reset
+        for (let i = 0; i < $shortcutDisplayList.length; i++){
+            $shortcutDisplayList[i].style.animation = "none";
+            setTimeout(function() { $shortcutDisplayList[i].style.animation = ''; }, 10);
+        }
+
+        listenerTimeout = setTimeout(function(){ listeningForShortcut = false; }, SHORTCUT_TIMEOUT);
+    }
+}
+
+function main(){
+    setupWelcomeMessage();
+    setupGroups();
+    document.addEventListener('keyup', shortcutListener, false);
+}
+
+main();
+
+window.onload = () => {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', 'http://api.openweathermap.org/data/2.5/weather?id=745044&units=metric&appid=e5b292ae2f9dae5f29e11499c2d82ece');
+    xhr.onload = () => {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                let json = JSON.parse(xhr.responseText);
+                const toTitleCase = (phrase) => {
+                    return phrase
+                        .toLowerCase()
+                        .split(' ')
+                        .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+                        .join(' ');
+                };
+                let loc = json.name;
+                document.getElementById("weather-description").innerHTML = 
+                loc + ', ' + toTitleCase(json.weather[0].description) + ', ' + json.main.temp.toFixed(0) + " C&#176;";
+            }
+        }
+    }
+    xhr.send();
+}
